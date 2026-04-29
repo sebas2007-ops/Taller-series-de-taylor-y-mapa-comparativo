36487246fc9f8a3a2d4fcf8e34b32beea0ad720f
EstebanValero404 <esteban2008games@gmail.com>
2026-04-29 08:18:44 -0500

    Add taylor.h

---
+++ taylor.h
@@ -0,0 +1,10 @@
+#ifndef TAYLOR_H
+#define TAYLOR_H
+//funciones
+double seno(double x, int n);
+double coseno(double x, int n);
+double raiz(double x, int n);
+double factorial(int n);
+double potencia(double base, int exponente);
+
+#endif
